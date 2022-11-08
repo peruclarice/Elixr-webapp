96d68cea22f89ae2585abd66d85f1834694e7cb9
Katumba <jonathanpkatumba@gmail.com>
2022-11-08 08:59:47 +0000

    Added a template of the blog route to be editted

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,12 @@ Route::get('/', function () {
     // return view('welcome');
     return view('home');
 });
+Route::get('/home', function () {
+    // return view('welcome');
+    return view('home');
+});
+
+Route::get('/blog', function () {
+    // return view('welcome');
+    return view('blog');
+});
