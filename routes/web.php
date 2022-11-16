65313418ac54a7ce7f075b11e8c25cabb173d914
Katumba <43390608+kpaool@users.noreply.github.com>
2022-11-16 07:26:28 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // return view('welcome');
+    
     $faqs= [
         'data'=>[
             [
@@ -31,6 +32,7 @@ Route::get('/', function () {
             ],
         ]        
     ];
+    $faqs=[];
     return view('home',$faqs);
 });
 Route::get('/home', function () {
