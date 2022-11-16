7b016fed7758185e0fba151ba9f6dea5c0d096a6
peru clarice <claricekibii@gmail.com>
2022-11-16 19:14:55 +0000

    plan modal added

---
+++ resources/views/layout/plans.blade.php
@@ -0,0 +1,81 @@
+<!-- Button trigger modal -->
+    {{-- <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#plansModal">
+        plans modal
+      </button> --}}
+  
+  <!-- Modal -->
+  <div class="modal fade" id="plansModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+      <div class="modal-content">
+        <div class="modal-header" style="flex-direction: column; justify-content: center;">
+            <h5 class="modal-title" id="exampleModalLabel">Elixr Plan</h5>
+            <p class="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed esse facilis sapiente vitae minima.</p>
+          {{-- <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button> --}}
+        </div>
+        <div class="modal-body">
+          <div class="row plan">
+            <div class="col-lg-6">
+                <div class="card starter-plan">
+                    <div class="card-header" style="background-color: transparent">
+                        <h5 class="card-title">Free</h5>
+                        <h6 class="per-month">0 <span>/per month</span></h6>
+                    </div>
+                    <div class="card-body">
+                        <ul>
+                            <li>
+                                <i class="fas fa-plus mx-2"></i>Lorem ipsum dolor sit amet.
+                            </li>
+                            <li>
+                                <i class="fas fa-plus mx-2"></i>Lorem ipsum dolor sit amet.
+                            </li>
+                            <li>
+                                <i class="fas fa-plus mx-2"></i>Lorem ipsum dolor sit amet.
+                            </li>
+                            <li>
+                                <i class="fas fa-plus mx-2"></i>Lorem ipsum dolor sit amet.
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="card-footer">
+                        <a href="#" class="btn btn-primary">TRY THIS PLAN OUT</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="card premium-plan">
+                    <div class="card-header">
+                        <h5 class="card-title">Elixr Diamond 💎</h5>
+                        <h6 class="per-month">20 <span>/per month</span></h6>
+                    </div>
+                    <div class="card-body">
+                        <ul>
+                            <li>
+                                <i class="fas fa-plus mx-2"></i>Lorem ipsum dolor sit amet.
+                            </li>
+                            <li>
+                                <i class="fas fa-plus mx-2"></i>Lorem ipsum dolor sit amet.
+                            </li>
+                            <li>
+                                <i class="fas fa-plus mx-2"></i>Lorem ipsum dolor sit amet.
+                            </li>
+                            <li>
+                                <i class="fas fa-plus mx-2"></i>Lorem ipsum dolor sit amet.
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="card-footer">
+                        <a href="#" class="btn btn-secondary">TRY THIS PLAN OUT</a>
+                    </div>
+                </div>
+            </div>
+          </div>
+        </div>
+        {{-- <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div> --}}
+      </div>
+    </div>
+  </div>
